7975c92597b84ee106170bcf6bc7bcce7fe8a735
1Frv <frcovazquez7845@gmail.com>
2023-12-07 12:26:37 -0600

    Se ha cargado el nuevo proyecto

---
+++ ProductorConsumidor.SinSincronizar/BufferImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProductorConsumidor.Modelo;
+namespace ProductorConsumidor.SinSincronizar
+{
+    public class BufferImpl : IBuffer
+    {
+        private int _buffer = -1; 
+        public int Buffer 
+        { get
+            { 
+                Console.WriteLine("{0} leyó el valor {1}", 
+                Thread.CurrentThread.Name, _buffer);
+                return _buffer; 
+            }
+            set
+            {
+                
+                _buffer = value; 
+                Console.WriteLine("{0} escribió el valor {1}",
+                Thread.CurrentThread.Name, value); 
+
+            }
+        }
+    }
+}
